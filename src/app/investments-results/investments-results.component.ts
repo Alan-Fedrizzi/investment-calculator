62095fe1bfc907a704aff62eb18453bd7e6f6a80
alanfedrizzi <alanfedrizzi@gmail.com>
2024-11-06 11:09:06 +0100

    outras maneitas no invest results

--- src/app/investments-results/investments-results.component.ts
+++ src/app/investments-results/investments-results.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
@@ -12,7 +12,10 @@ import { InvestmentService } from '../investment.service';
 export class InvestmentsResultsComponent {
   private investmentService = inject(InvestmentService);
 
-  get results() {
-    return this.investmentService.resultData;
-  }
+  // funciona das 3 maneiras abaixo:
+  // get results() {
+  //   return this.investmentService.resultData;
+  // }
+  results = computed(() => this.investmentService.resultData());
+  // results = this.investmentService.resultData.asReadonly();
 }
